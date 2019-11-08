af8505078144034056817bd340ff0d7a92377456
Andy Bell <andy.bell@displaynote.com>
2019-11-08 14:20:28 +0100

    Call Rust function from JS and then back from Rust JS - Bridge

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+// Our JS function we will call
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+// This allows this to be called by Javascript
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
