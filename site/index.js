28cbe989440167d412a951b928bbb587932f5eda
Andy Bell <andy.bell@displaynote.com>
2019-11-08 14:46:00 +0100

    Fix path

--- site/index.js
+++ site/index.js
@@ -1,4 +1,4 @@
-const js = import("./node_modules/@andyjsbell/simple-wasm/simple-wasm.js");
+const js = import("./node_modules/@andyjsbell/simple-wasm/simple_wasm.js");
 js.then(js => {
     js.greet("Simple Rust with WebAssembly");
 });
